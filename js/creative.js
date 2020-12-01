ce7909a54cfa09b82214b6c8714aac4923952515
Ryan <59742677+privateryann1@users.noreply.github.com>
2020-12-01 15:55:49 -0800

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -4,6 +4,10 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
 (function($) {
     "use strict"; // Start of use strict
 
